7c5b15bca7ec562c07a4daf1bc0bc96b0b4fe786
Ali Hamza Malik <88540402+alirafiqmalik@users.noreply.github.com>
2021-08-21 02:39:19 +0500

    Update pdf_to_dataset.py

--- pdf_to_dataset.py
+++ pdf_to_dataset.py
@@ -1,17 +1,29 @@
-import os
-from os.path import isfile, join
-
-from io import StringIO
-
-from pdfminer.converter import TextConverter
-from pdfminer.layout import LAParams
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-from pdfminer.pdfpage import PDFPage
+from pdfminer.converter import TextConverter
 from pdfminer.pdfdocument import PDFDocument
 from pdfminer.pdfparser import PDFParser
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+from multiprocessing import Process
+from os.path import isfile, join
+from io import StringIO
+
+import time
+import os
 
 
-import csv
+
+
+foldername="legalpdf/"
+txtpath="txtfiles/"
+
+
+def createdir(path):
+  try:
+    os.mkdir(path)
+  except OSError as er:
+    print(er)
+
 
 
 def convert_pdf_to_string(file_path):
@@ -30,44 +42,49 @@ def convert_pdf_to_string(file_path):
 
 
 
-foldername="legalpdf/"
-txtpath="txtfiles/"
-
-
-def createdir(path):
-  try:
-    os.mkdir(path)
-  except OSError as er:
-    print(er)
-
-
 def pdf_to_txt(x):
   try:
     filepath=foldername+x
     text=convert_pdf_to_string(filepath)
     dstfile=txtpath+x[0:-4]+".txt"
-    print("File %s Converted to %s"%(filepath, dstfile))
     file=open(dstfile,"w")
     file.writelines(text)
     file.close()
-  except OSError as er:
-    print(er)
+    print("File %s Converted to %s" % (filepath, dstfile))
+  except:
+    print("########################   ERROR   ########################")
 
 
-onlyfiles = [f for f in os.listdir(foldername) if isfile(join(foldername, f))]
-print(len(onlyfiles))
+IncompleteError=[]
+def multiprocess(f,tasks,timeout = 120,pool_len = 60):
+  procs = []
+  while len(tasks) > 0 or len(procs) > 0:
+    if len(tasks) > 0 and len(procs) < pool_len:
+      n = tasks.pop(0)
+      p = Process(target=f, args=(n,))
+      p.start()
+      procs.append({'n': n, 'p': p, 't': time.time() + timeout})
+    for d in procs:
+      if not d['p'].is_alive():
+        procs.remove(d)
+        #print ('%s'% d['n'])
+      elif d['t'] < time.time():
+        d['p'].terminate()
+        procs.remove(d)
+        IncompleteError.append(d['n'])
+        print( 'Timeout Process Killed for = %s ' % d['n'])
+    time.sleep(0.05)
 
-createdir(txtpath)
-
-filepath='legalpdf/2011LHC4000.pdf'
 
 
-text = convert_pdf_to_string(filepath)
-print(text)
+if __name__ == "__main__":
+  onlyfiles = [f for f in os.listdir(foldername) if isfile(join(foldername, f))]
+  print(len(onlyfiles))
 
+  createdir(txtpath)
 
+  #filepath = 'legalpdf/2011LHC4000.pdf'
 
-#for i in onlyfiles:
-#  pdf_to_txt(i)
+  multiprocess(pdf_to_txt,onlyfiles,pool_len = 60)
 
 
